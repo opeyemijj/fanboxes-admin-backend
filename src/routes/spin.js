4c3bdd2ae9eb64b82d7bbc22986974ece02d2969
shahadath-shoutty <a.shahadath@shoutty.app>
2025-08-19 19:50:46 +0600

    changed the route name for getting spins

--- src/routes/spin.js
+++ src/routes/spin.js
@@ -5,6 +5,6 @@ const verifyToken = require("../config/jwt");
 
 router.post("/admin/spin", verifyToken, spin.createSpinByAdmin);
 router.post("/admin/spin-verify", verifyToken, spin.spinVerify);
-router.get("/admin/get-spins", verifyToken, spin.getSpinsByAdmin);
+router.get("/admin/spins", verifyToken, spin.getSpinsByAdmin);
 
 module.exports = router;
