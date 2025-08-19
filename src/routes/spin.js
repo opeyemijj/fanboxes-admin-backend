5cec697b2d831eb672d6c0c41ee41d3b816a7b3f
shahadath-shoutty <a.shahadath@shoutty.app>
2025-08-19 16:49:55 +0600

    birng verify token in spin api

--- src/routes/spin.js
+++ src/routes/spin.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const spin = require("../controllers/spin");
+const verifyToken = require("../config/jwt");
 
-router.post("/admin/spin", spin.createSpinByAdmin);
-router.post("/admin/spin-verify", spin.spinVerify);
+router.post("/admin/spin", verifyToken, spin.createSpinByAdmin);
+router.post("/admin/spin-verify", verifyToken, spin.spinVerify);
 
 module.exports = router;
