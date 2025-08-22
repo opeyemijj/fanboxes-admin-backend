c909d0dd49cde965497aef6f5b0f70cc0f327a10
shahadath-shoutty <a.shahadath@shoutty.app>
2025-08-22 15:33:56 +0600

    made the shop query data date sort

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -539,14 +539,9 @@ const getShops = async (req, res) => {
     page = parseInt(page) || 1; // default page to 1 if not provided
     limit = parseInt(limit) || null; // default limit to null if not provided
 
-    let shopsQuery = Shop.find().select([
-      "products",
-      "slug",
-      "title",
-      "logo",
-      "cover",
-      "followers",
-    ]);
+    let shopsQuery = Shop.find()
+      .select(["products", "slug", "title", "logo", "cover", "followers"])
+      .sort({ createdAt: -1 });
 
     // Apply pagination only if limit is provided
     if (limit) {
